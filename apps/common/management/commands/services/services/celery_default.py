88fc5dd39835c2bc369a966d799d930f8d8b3820
shaohuzhang1 <80892890+shaohuzhang1@users.noreply.github.com>
2025-11-06 10:31:14 +0800

    fix: Set the startup environment variables (#4321)

--- apps/common/management/commands/services/services/celery_default.py
+++ apps/common/management/commands/services/services/celery_default.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+
 from .celery_base import CeleryBaseService
 
 __all__ = ['CeleryDefaultService']
@@ -8,3 +11,14 @@ class CeleryDefaultService(CeleryBaseService):
     def __init__(self, **kwargs):
         kwargs['queue'] = 'celery'
         super().__init__(**kwargs)
+
+    def open_subprocess(self):
+        env = os.environ.copy()
+        env['SERVER_NAME'] = 'celery'
+        kwargs = {
+            'cwd': self.cwd,
+            'stderr': self.log_file,
+            'stdout': self.log_file,
+            'env': env
+        }
+        self._process = subprocess.Popen(self.cmd, **kwargs)
